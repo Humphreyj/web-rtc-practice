832e791ceca879dec5c554b43877f687d60f2aa6
HumphreyJ <joshhumphrey1@gmail.com>
2020-06-10 16:41:37 -0500

    The chat is styled a little bit and admin messaging improved.

--- server/index.js
+++ server/index.js
@@ -5,10 +5,11 @@ const http = require('http');
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
 const app = express();
+const cors = require('cors')
 const server = http.createServer(app);
 const io = socketio(server);
-const {addUser, removeUser, getUser, getUsersInroom} = require('./users');
-
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users');
+app.use(cors())
 app.use(router);
 
 io.on('connection',(socket)=> {
@@ -21,9 +22,10 @@ io.on('connection',(socket)=> {
         if(error) return callback(error);
 
         socket.emit('message', {user:'admin', text:`${user.name} welcome to the room ${user.room}`});
-        socket.broadcast.to(user.room).emit('message', {name: 'admin', text: `${user.name} has joined!`})
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined!`})
 
         socket.join(user.room);
+        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
 
         callback();
     })
@@ -32,11 +34,18 @@ io.on('connection',(socket)=> {
         const user = getUser(socket.id)
 
         io.to(user.room).emit('message', {user: user.name, text: message});
+        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
         callback();
     })
 
     socket.on('disconnect',()=> {
-        console.log('a user has left.')
+        const user = removeUser(socket.id)
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+          }
+
+
     })
 })
 
